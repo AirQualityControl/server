f51fdf5e90b51749cadc2b6bd24886a3d38d3372
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2021-04-16 01:36:29 +0300

    Added controllers with  thoughtless stubs

---
+++ src/AirQ.API/Controllers/AirPolutionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirQ.API.Controllers
+{
+    [ApiController]
+    [Route("airpolution")]
+    public class AirPolutionController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get(double lat, double lng, double radius)
+        {
+            return await Task.FromResult(Ok($"Air polution stub for lat: {lat}," +
+                $" lng: {lng}, radius: {radius}"));
+        }
+    }
+}
---
+++ src/AirQ.API/Controllers/AirmonitoringStationsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirQ.API.Controllers
+{
+    [ApiController]
+    [Route("airmonitoringstations")]
+    public class AirmonitoringStationsController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult> GetPaginated(int limit, int offset)
+        {
+            return await Task.FromResult(Ok($"ListOfStations limit: {limit}, offset: {offset}"));
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> GetWithIncludes(int id, [FromQuery] string include)
+        {
+            if (!String.IsNullOrEmpty(include))
+            {
+                return await Task.FromResult(Ok($"stationId = {id} include = " +
+                    include.Trim().Split(',').Aggregate((x, y) => $"{x};{y}")));
+            }
+            return await Task.FromResult(Ok($"stationId = {id}"));
+        }
+
+        [HttpGet]
+        [Route("{id}/airPollution")]
+        public async Task<ActionResult> GetAirPolution(int id)
+        {
+            return await Task.FromResult(Ok($"airpolution info from stationId = {id}"));
+        }
+
+        [HttpGet]
+        [Route("{id}/dataprovider")]
+        public async Task<ActionResult> GetDataProvider(int id)
+        {
+            return await Task.FromResult(Ok($"dataprovier info from stationId = {id}"));
+        }
+
+        [HttpGet]
+        [Route("{id}/airPollutionHistory")]
+        public async Task<ActionResult> GetAirPolutionHistory(int id)
+        {
+            return await Task.FromResult(Ok($"Air polution history info from stationId = {id}"));
+        }
+
+        [HttpGet]
+        [Route("{id}/city")]
+        public async Task<ActionResult> GetCity(int id)
+        {
+            return await Task.FromResult(Ok($"City info from stationId = {id}"));
+        }
+
+    }
+}
