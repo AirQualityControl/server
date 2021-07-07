8b2dc45fe12d32de752b692ed4faca3aa3512041
Artur Lavrov <arturstylus@gmail.com>
2021-07-08 00:38:07 +0200

    recreate reference to DI

--- src/AirSnitch.API/Startup.cs
+++ src/AirSnitch.API/Startup.cs
@@ -1,11 +1,11 @@
 using AirSnitch.Api.Extensions;
+using AirSnitch.Di;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using AirSnitch.Di;
 
 namespace AirSnitch.Api
 {
