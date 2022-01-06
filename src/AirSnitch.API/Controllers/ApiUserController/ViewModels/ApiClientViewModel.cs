263699e6be543b84e14b6063079b0dd5a0b1e8ae
Artur Lavrov <arturstylus@gmail.com>
2022-01-06 23:02:43 +0200

    fix viewmodel formatting

--- src/AirSnitch.API/Controllers/ApiUserController/ViewModels/ApiClientViewModel.cs
+++ src/AirSnitch.API/Controllers/ApiUserController/ViewModels/ApiClientViewModel.cs
@@ -12,34 +12,30 @@ namespace AirSnitch.Api.Controllers.ApiUserController.ViewModels
     {
         private readonly IReadOnlyCollection<ApiClient> _clients;
         private readonly HttpRequest _request;
-        private readonly Dictionary<string, object> _resultDictionary;
-
         public ApiClientViewModel(IReadOnlyCollection<ApiClient> clients, HttpRequest request)
         {
             _clients = clients;
             _request = request;
-            _resultDictionary = new Dictionary<string, object>()
-            {
-                {"values", null},
-            };
+            
         }
 
         public QueryResult GetResult()
         {
-            _resultDictionary["values"] = new JArray(
-                _clients.
-                    Select(c => new JObject(
-                            new JProperty("id", c.Id), 
-                            new JProperty("createdOn",c.CreatedOn),
-                            new JProperty("name",c.Name.Value),
-                            new JProperty("description",c.Status),
-                            new JProperty("type",c.Type)
-                        )
-                    )
-            );
-           
+            var resultDictionary = new Dictionary<string, object>()
+            {
+                {"values", null},
+            };
+            
+            foreach (var client in _clients)
+            {
+                resultDictionary["id"] = client.Id;
+                resultDictionary["createdOn"] = client.CreatedOn;
+                resultDictionary["name"] = client.Name;
+                resultDictionary["description"] = client.Description;
+                resultDictionary["type"] = client.Type;
+            }
             return new QueryResult(
-                new List<Dictionary<string, object>>(){_resultDictionary},
+                new List<Dictionary<string, object>>(){resultDictionary},
                 new AirQualityIndexResponseFormatter()
             );
         }
