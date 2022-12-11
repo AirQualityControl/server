f78c712e61e4e31e7ca5869906467f6a30ed3a4f
Artur Lavrov <arturstylus@gmail.com>
2022-12-11 12:10:31 +0200

    remove settings print from stdout

--- src/AirSnitch.Infrastructure/MessageQueue/AmazonSqsSettings.cs
+++ src/AirSnitch.Infrastructure/MessageQueue/AmazonSqsSettings.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace AirSnitch.Infrastructure.MessageQueue
 {
     /// <summary>
@@ -8,37 +5,13 @@ namespace AirSnitch.Infrastructure.MessageQueue
     /// </summary>
     public class AmazonSqsSettings
     {
-        private string _queueUrl;
-        public string QueueUrl {
-            get
-            {
-                return _queueUrl;
-            }
-            set
-            {
-                _queueUrl = value;
-                Console.WriteLine($"This is a value {value}");
-            }
-        }
+        public string QueueUrl { get; set; }
 
         public string ServiceUrl { get; set; }
 
         public string AccessKey { get; set; }
+        public string AccessSecrete { get; set; }
 
-        private string _accessSecrete;
-        public string AccessSecrete {
-            get
-            {
-                return _accessSecrete;
-            }
-            set
-            {
-                _accessSecrete = value;
-                Console.WriteLine($"This is a secrete {_accessSecrete}");
-                
-            }
-        }
-        
         public int RequestTimeOut { get; set; }
         
         public int MaxNumberOfMessagesInRequest { get; set; }
