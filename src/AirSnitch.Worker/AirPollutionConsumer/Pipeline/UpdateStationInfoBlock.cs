049090ffd776dc5d9e6815b2d2433c40117db518
Artur Lavrov <arturstylus@gmail.com>
2023-12-02 13:21:55 +0200

    improve station update logic

--- src/AirSnitch.Worker/AirPollutionConsumer/Pipeline/UpdateStationInfoBlock.cs
+++ src/AirSnitch.Worker/AirPollutionConsumer/Pipeline/UpdateStationInfoBlock.cs
@@ -4,14 +4,19 @@ using System.Threading.Tasks.Dataflow;
 using AirSnitch.Domain.Models;
 using AirSnitch.Infrastructure.Abstract.MessageQueue;
 using AirSnitch.Infrastructure.Abstract.Persistence.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace AirSnitch.Worker.AirPollutionConsumer.Pipeline
 {
     public class UpdateStationInfoBlock
     {
         private readonly IMonitoringStationRepository _monitoringStationRepository;
-        public UpdateStationInfoBlock(IMonitoringStationRepository monitoringStationRepository)
+        private readonly ILogger<AirPollutionDataConsumer> _logger;
+        public UpdateStationInfoBlock(
+            IMonitoringStationRepository monitoringStationRepository, 
+            ILogger<AirPollutionDataConsumer> logger)
         {
+            _logger = logger;
             _monitoringStationRepository = monitoringStationRepository;
         }
         
@@ -23,14 +28,35 @@ namespace AirSnitch.Worker.AirPollutionConsumer.Pipeline
             var existingStation = await _monitoringStationRepository.FindByProviderNameAsync(monitoringStation.DisplayName);
             if (existingStation.IsEmpty)
             {
+                _logger.LogInformation($"new station was added {monitoringStation.DisplayName}");
                 await _monitoringStationRepository.AddAsync(monitoringStation);
                 return tuple.Item1;
             }
 
+            var existingAirPollution = existingStation.GetAirPollution();
+            if(existingAirPollution == null)
+            {
+                _logger.LogWarning($"Air pollution for existing station {existingStation.DisplayName} is empty");
+                await UpdateStation(monitoringStation, existingStation);
+            }
+
+            var existingStationDateTime = existingStation.GetAirPollution()?.GetMeasurementsDateTime();
+            var receivedStationDateTime = monitoringStation.GetAirPollution()?.GetMeasurementsDateTime();
+
+            if (receivedStationDateTime > existingStationDateTime)
+            {
+                await UpdateStation(monitoringStation, existingStation);
+                _logger.LogInformation($"A new monitoring data for station {monitoringStation.DisplayName} received. New measurement date: {receivedStationDateTime}");
+            }
+            
+            return tuple.Item1;
+        }
+
+        private async Task UpdateStation(MonitoringStation monitoringStation, MonitoringStation existingStation)
+        {
             monitoringStation.Id = existingStation.Id;
             monitoringStation.PrimaryKey = existingStation.PrimaryKey;
             await _monitoringStationRepository.UpdateAsync(monitoringStation);
-            return tuple.Item1;
         }
     }
 }
